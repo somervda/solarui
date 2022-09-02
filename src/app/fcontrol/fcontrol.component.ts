60db7087602ca5e445297da1832eb8a90e85a80a
David <39265971+somervda@users.noreply.github.com>
2022-09-01 21:57:45 -0400

    wip

--- src/app/fcontrol/fcontrol.component.ts
+++ src/app/fcontrol/fcontrol.component.ts
@@ -7,6 +7,13 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class FcontrolComponent implements OnInit {
   @Input() value: string = '';
+  value100000000 = '';
+  value10000000 = '';
+  value1000000 = '';
+  value100000 = '';
+  value10000 = '';
+  value1000 = '';
+  value100 = '';
   value10 = '';
   value1 = '';
 
@@ -16,16 +23,28 @@ export class FcontrolComponent implements OnInit {
     this.setDigits();
   }
 
-  updateFrequency($event: number) {
-    console.log($event);
-    this.value = (parseInt(this.value) + $event).toString();
+  updateFrequency(updateAmount: number) {
+    console.log(updateAmount);
+    let value = parseInt(this.value) + updateAmount;
+    this.value = this.padWithZero(value, 9);
     console.log(this.value);
     this.setDigits();
   }
 
   setDigits() {
-    this.value10 = this.value.substring(0, 1);
-    this.value1 = this.value.substring(1, 2);
+    this.value100000000 = this.value.substring(0, 1);
+    this.value10000000 = this.value.substring(1, 2);
+    this.value1000000 = this.value.substring(2, 3);
+    this.value100000 = this.value.substring(3, 4);
+    this.value10000 = this.value.substring(4, 5);
+    this.value1000 = this.value.substring(5, 6);
+    this.value100 = this.value.substring(6, 7);
+    this.value10 = this.value.substring(7, 8);
+    this.value1 = this.value.substring(8, 9);
     console.log('Value10:', this.value10, ' Value1:', this.value1);
+  }
+
+  padWithZero(num: number, targetLength: number) {
+    return String(num).padStart(targetLength, '0');
   }
 }
